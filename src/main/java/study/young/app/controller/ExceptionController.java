ccdba220de34d52e7d6d2d540bd8e9fa3ee0a5ea
youngtin <youngtin1017@gmail.com>
2020-02-22 12:03:23 +0800

    test

--- src/main/java/study/young/app/controller/ExceptionController.java
+++ src/main/java/study/young/app/controller/ExceptionController.java
@@ -10,7 +10,7 @@ public class ExceptionController {
 	// 当用户访问未授权的资源时，跳转到 unauthorized 视图，并携带出错信息
 	@ExceptionHandler(AuthorizationException.class)
 	public ModelAndView error(AuthorizationException e) {
-		ModelAndView mv = new ModelAndView("unauthorized");
+		ModelAndView mv = new ModelAndView("unauthorized"); 
 		mv.addObject("error", e.getMessage());
 		return mv;
 	}
